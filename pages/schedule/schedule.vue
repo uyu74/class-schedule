7158fd09eb5de0312ca37cf3c594adc46e72577c
FU YONGQI <148615767+uyu74@users.noreply.github.com>
2024-11-16 21:42:36 +0800

    增加了课程表页面进入课程表全局设置的按钮

--- pages/schedule/schedule.vue
+++ pages/schedule/schedule.vue
@@ -2,18 +2,22 @@
   <view class="container">
     <!-- 页面内容 -->
     <view class="content">
-      <text>这里是页面内容</text>
+      <!-- 课程表内容 --> <!-- 待完成 -->
     </view>
-
+    
     <!-- 底部按钮 -->
-    <view class="bottom-btn">
-      <button @click="goToNextPage">跳转到课程表</button>
+    <view class="bottom-btn-container">
+      <view class="bottom-btn-add-course" @click="goToNextPage">添加课程</view>
+      <view class="bottom-btn-schedule-setting">设置日程</view>
     </view>
   </view>
 </template>
 
 <script>
 export default {
+  data() {
+    return {};
+  },
   methods: {
     // 跳转到课程表页面
     goToNextPage() {
@@ -25,7 +29,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 .container {
   padding: 20px;
 }
@@ -34,24 +38,36 @@ export default {
   margin-bottom: 100px; /* 留出底部按钮的空间 */
 }
 
-.bottom-btn {
+.bottom-btn-container {
   position: fixed;
   bottom: 20px;
   left: 50%;
   transform: translateX(-50%);
+  display: flex;
+  justify-content: space-between;
+  width: 200px;
   z-index: 10;
 }
 
-button {
-  padding: 10px 20px;
+.bottom-btn-add-course, .bottom-btn-schedule-setting {
   background-color: #1c97f7;
   color: white;
-  border: none;
+  padding: 10px 20px;
   border-radius: 20px;
   font-size: 16px;
+  text-align: center;
 }
 
-button:hover {
+.bottom-btn-add-course {
+  margin-right: 10px; /* 按钮间距 */
+}
+
+.bottom-btn-schedule-setting {
+  background-color: #1873b0;
+}
+
+/* 按钮点击样式 */
+.bottom-btn-add-course:hover, .bottom-btn-schedule-setting:hover {
   background-color: #1873b0;
 }
 </style>
--- unpackage/dist/dev/mp-weixin/pages/schedule/schedule.js
+++ unpackage/dist/dev/mp-weixin/pages/schedule/schedule.js
@@ -1,6 +1,9 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
+  data() {
+    return {};
+  },
   methods: {
     // 跳转到课程表页面
     goToNextPage() {
@@ -16,5 +19,5 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
     a: common_vendor.o((...args) => $options.goToNextPage && $options.goToNextPage(...args))
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-e6e5e79f"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
 wx.createPage(MiniProgramPage);
